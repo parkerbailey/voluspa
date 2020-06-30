0a6e36e999d6fc01c8abe6617a505e04eca51cef
parkerbailey <parkerreedbailey@gmail.com>
2020-06-30 17:14:04 -0400

    add in command handler and a sample greeting command

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
-// Import Discord module
+// require discord.js module and config file
 const Discord = require('discord.js');
+const { prefix, token } = require('./config.json');
 
-// Initialize client object
+// create new Discord client
 const client = new Discord.Client();
 
-// Bot startup message
-client.on('ready', () => {
+// welcome message
+client.once('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
-// Bot message read
-client.on('message', msg => {
-    if (msg.content === '>Hello') {
-        msg.reply('Greetings Guardian.')
+// command handler
+client.on('message', message => {
+    // slice user input into space-seperated array
+    const args = message.content.slice(prefix.length)
+    // strips off prefix and removes case sensitivity
+    const command = args.shift().toLowerCase();
+    // ignore message that don't contain the prefix or are from a bot
+    if (!message.content.startsWith(prefix) || message.author.bot) {
+        return;
+    }
+    // conditional statements for commands
+    else if (command === 'hello' || 'hi') {
+        return message.channel.send('Greetings Guardian.')
     }
 });
 
 // token login
-client.login('NzI3NTI3NzI4NDk1OTg0Njkw.Xvt4lQ.QUIwQRhZYuXTrgqYgkZ1JrF8BLY');
+client.login(token);
